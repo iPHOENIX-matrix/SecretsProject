a3357e5ae7ff6c61133e904681cb0702875ccdb8
iPHOENIX <suvrajeetjash@gmail.com>
2023-07-22 22:07:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+
+import express from "express";
+import bodyParser from "body-parser";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var allowUser = false;
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+function checking(req,res,next){
+    const password = req.body["password"];
+    if(password==="Jash")
+    {
+        allowUser=true;
+    }
+    next();
+}
+
+app.use(checking);
+
+app.get("/",(req,res)=>{
+res.sendFile(__dirname+"/public/index.html");
+});
+
+app.post("/check",(req,res)=>{
+    if(allowUser){
+        res.sendFile(__dirname+"/public/secret.html");
+    }
+    else{
+        res.sendFile(__dirname+"/public/index.html");
+    }
+    });
+
+app.listen(port, ()=>{
+console.log('Server running on 3000');
+});
